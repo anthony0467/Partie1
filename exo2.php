56e3b0c3c58b3d1ae384033c73c9d215ad290601
Anthony Spiteri <anthonyspiteri@hotmail.fr>
2023-02-07 10:56:06 +0100

    test

--- exo2.php
+++ exo2.php
@@ -2,7 +2,6 @@
 
 <p>A partir de la phrase de l’exercice 1, écrire l’instruction permettant de compter le nombre de mots contenus dans celle-ci.</p>
 
-<h2>Résultat exo 2</h2>
 
 <?php 
 $phrase = "Notre formation DL commence aujourd'hui";
